1261e6abca06ee39b37d755fbc9f00d28eef2324
almaro24-meet <110034583+almaro24-meet@users.noreply.github.com>
2023-08-01 12:15:54 +0300

    1

--- app.py
+++ app.py
@@ -44,12 +44,10 @@ def signup():
         email = request.form['email']
         password = request.form['password']
         disability = request.form['disability']
-        experience = request.form['experience']
-        interests = request.form['interests']
         availability = request.form['availability']
         try:
             login_session['user'] = auth.create_user_with_email_and_password(email, password)
-            user = {"full_name" : full_name, "email" : email, "password" : password, "disability" : disability, "experience" : experience, "interests" : interests, "availability" : availability}
+            user = {"full_name" : full_name, "email" : email, "password" : password, "disability" : disability, "availability" : availability}
             UID = login_session['user']['localId']
             db.child("Users").child(UID).set(user)
             return redirect(url_for('home'))
