1ec682a89f619bebde381a2397fc51a3d808bee9
almaro24-meet <110034583+almaro24-meet@users.noreply.github.com>
2023-08-01 16:13:21 +0300

    1

--- app.py
+++ app.py
@@ -88,7 +88,8 @@ def nitting():
 @app.route('/volunteers', methods=['GET', 'POST'])
 def volunteers():
     error = ""
-    if request.method == 'POST':
+    UID_user = login_session['user']['localId']
+    if request.method == 'POST' and UID_user!=None:
         full_name = request.form['full_name']
         email = request.form['email']
         password = request.form['password']
@@ -98,7 +99,20 @@ def volunteers():
             login_session['user'] = auth.create_user_with_email_and_password(email, password)
             user = {"full_name" : full_name, "email" : email, "password" : password, "program" : program, "availability" : availability}
             UID = login_session['user']['localId']
-            db.child("volunteers").child(UID).set(user)
+            db.child("Volunteers").child(UID).set(user)
+            return redirect(url_for('home'))
+        except:
+            error = "Authentication failed"
+    return render_template("volunteers.html")
+
+@app.route('/volunteers2', methods=['GET', 'POST'])
+def volunteers2():
+    error = ""
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        try:
+            login_session['user'] = auth.sign_in_with_email_and_password(email, password)
             return redirect(url_for('home'))
         except:
             error = "Authentication failed"
